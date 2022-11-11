f959440ded868facac0dd35befb97ada825918fd
jesse berkowitz <jberkow@brandeis.edu>
2022-11-10 20:08:42 -0800

    checking open spots func

--- BG2.py
+++ BG2.py
@@ -1,4 +1,10 @@
-import random
+import numpy as np
+import random 
+import pygame as p
+from pygame.constants import MOUSEBUTTONDOWN
+import sys
+import itertools
+import copy
 
 class Board:
     def __init__(self):
@@ -11,6 +17,7 @@ class Player:
         self.color = color
         self.board = Board().board
         self.on_rail = False
+        self.can_remove = False
         self.dice = None
         self.Comp = Comp
         self.White_Pieces = {}
@@ -44,8 +51,38 @@ class Player:
                 self.White_Pieces[slot]=len(val)
             elif 2 in val:
                 self.Black_Pieces[slot]=len(val)
-
+    
+    def blocked(self,spot):
+        if self.color == 'white':
+            if spot in self.Black_Pieces:
+                if self.Black_Pieces[spot]>1:
+                    return True
+            return False      
+        if self.color =='black':
+            if spot in self.White_Pieces:
+                if self.White_Pieces[spot]>1:
+                    return True
+            return False     
+    
+    def spot_open(self, spot):
+        if self.color =='white':
+            if self.blocked(spot)==True:
+                return False
+            if self.can_remove == False:
+                if spot>=25:
+                    return False                
+            return True
+        if self.color =='black':
+            if self.blocked(spot)==True:
+                return False
+            if self.can_remove == False:
+                if spot<=0:
+                    return False                
+            return True                     
+               
 P1 = Player('white')
-print(P1.White_Pieces, P1.Black_Pieces)                    
-
+P2 = Player('black')
+print(P1.White_Pieces, P1.Black_Pieces)
+print(P1.spot_open(6))                    
+print(P2.spot_open(18))
                 

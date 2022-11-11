3cca7524e92d02681511ccc8d35dd292676e2f36
jesse berkowitz <jberkow@brandeis.edu>
2022-11-11 12:56:15 -0800

    visuals begun

--- BG2.py
+++ BG2.py
@@ -6,10 +6,46 @@ import sys
 import itertools
 import copy
 
+p.init()
+width = 1200
+height = 800 
+FPS =60
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+PURPLE = (255,0,255)
+BROWN = (139,131,120)
+
+clock = p.time.Clock()
+screen = p.display.set_mode((width, height))
+p.display.set_caption("Backgammon")
+
+screen.fill(WHITE)
+
 class Board:
     def __init__(self):
         self.board = [[],[1,1],[], [],[],[],[2,2,2,2,2], [],[2,2,2],[],[],[],[1,1,1,1,1],\
             [2,2,2,2,2],[],[],[],[1,1,1],[], [1,1,1,1,1], [],[],[],[],[2,2],[]]
+        self.buffer = 50
+        self.draw_board()    
+    def draw_board(self):
+        start_x, start_y = self.buffer,self.buffer 
+        end_x = width-self.buffer
+        end_y = height-self.buffer        
+        p.draw.line(screen, BLACK, (start_x, start_y), (end_x, start_y))
+        p.draw.line(screen, BLACK, (start_x, end_y), (end_x, end_y))
+        p.draw.line(screen, BLACK, (start_x, start_y), (start_x, end_y))
+        p.draw.line(screen, BLACK, (end_x, start_y), (end_x, end_y))
+        x_gap = (end_x-start_x)/15
+        for i in range(15):
+            p.draw.line(screen, BLACK, (start_x+i*x_gap, start_y), (start_x+i*x_gap, end_y))
+        p.draw.rect(screen,BROWN,(start_x,start_y,x_gap,end_y-start_y))
+        p.draw.rect(screen,BROWN,(start_x+x_gap*7,start_y,x_gap,end_y-start_y))
+        p.draw.rect(screen,BROWN,(start_x+x_gap*14,start_y,x_gap+1,end_y-start_y))
+    def draw_pieces(self):
+        pass       
 
 class Player:
     # Players will use the same starting board, and as moves are made, the board will be adjusted
@@ -100,13 +136,25 @@ class Player:
         return
 
 P1 = Player('white')
-print(P1.White_Pieces, P1.Black_Pieces)
-P1.roll()
-P1.calc_moves(1)
-P1.rail_check()
-print(P1.on_rail)
+# print(P1.White_Pieces, P1.Black_Pieces)
+# P1.roll()
+# P1.calc_moves(1)
+# P1.rail_check()
+# print(P1.on_rail)
 
 
+running = True
+while running:
+    
+    clock.tick(FPS)
+    
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            sys.exit()
+    # if event.type == p.KEYDOWN:       
+    
+    
+    p.display.flip()
 
 
                 

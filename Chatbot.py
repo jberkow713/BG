91e3726cf7dfabf71793c8223e3c2f3346511ea3
jesse berkowitz <jberkow@brandeis.edu>
2022-12-24 16:10:10 -0800

    tense specified func

--- Chatbot.py
+++ Chatbot.py
@@ -2,20 +2,29 @@ import nltk
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
+import spacy
 import copy
 import torch
 from transformers import BertTokenizer, BertModel
 import numpy as np
 from scipy import spatial
 
+nlp = spacy.load("en_core_web_sm")
 model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+bert = BertTokenizer.from_pretrained('bert-base-uncased')
 model.eval()
+words = [x for x in bert.vocab.keys()]
+
+def find_types(word_list, tenses):
+    # Finds tense specified words from given word list and list of tenses    
+    doc = nlp(' '.join(word_list))
+    return [x.text for x in doc if x.text.isalpha()==True and len(x.text)>1 and x.pos_ in tenses]    
+
+    # print(find_types(words, ['NOUN', 'VERB', 'ADJ']))
 
 def bert_vectors(sent):
     # Creates contextualized vectors for given sentence
-    d = {}
-    bert = BertTokenizer.from_pretrained('bert-base-uncased')    
-    words = [x for x in bert.vocab.keys()]    
+    d = {}      
     tokenized_text = bert.tokenize(sent)
     token_ids = bert.convert_tokens_to_ids(tokenized_text)
     tokens_tensor = torch.tensor([token_ids])    
@@ -30,14 +39,16 @@ def bert_vectors(sent):
     
     return d
 
-A = bert_vectors('pizza is tasty')
-B = bert_vectors('i like pizza')
+A = bert_vectors('i like to eat pizza')
+B = bert_vectors('eat some cheese it tastes good')
+
+# consider clustering based on vector
 
 def compare_words(a,b):
     #compares word vectors using Bert model 
     return 1 - spatial.distance.cosine(a,b)
 
-print(compare_words(A['pizza'], B['pizza']))
+print(compare_words(A['pizza'], B['cheese']))
 
 # TODO
 # Language converter

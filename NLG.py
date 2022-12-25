5f19a0c7bf5af4b85dbf283a5110ac556602f624
jesse berkowitz <jberkow@brandeis.edu>
2022-12-25 10:38:27 -0800

    stuff

--- NLG.py
+++ NLG.py
@@ -3,9 +3,11 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import string
 import random
 import numpy as np
+from scipy import spatial
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
+
 
 def remove_punc(sent):
     # Removes punctuation given sentence
@@ -17,6 +19,10 @@ def get_vector(word):
     text_index = tokenizer.encode(word,add_prefix_space=True)
     vector = model.transformer.wte.weight[text_index,:]
     return vector[0].detach().numpy()
+def compare_words(a,b):
+    #compares word vectors using Bert model 
+    return 1 - spatial.distance.cosine(a,b)
+
 
 class Chatbot:
     # Initial version of chatbot
@@ -25,7 +31,7 @@ class Chatbot:
         self.min_reply = min_reply
     def generate_response(self):
         inputs = tokenizer.encode(self.input, return_tensors='pt')
-        outputs = model.generate(inputs, max_length=100, do_sample=True, temperature=.3,top_k=50)
+        outputs = model.generate(inputs, max_length=50, do_sample=True, temperature=3.0,top_k=50)
         text = tokenizer.decode(outputs[0], skip_special_tokens=True)
         original = list(set(text.split('.')))        
         final = [x for x in original if x!=remove_punc(self.input) and len(x)>0 and '\n' not in x and len(x.split())>self.min_reply]
@@ -35,9 +41,11 @@ class Chatbot:
         rand_response = final[random.randint(0,len(final)-1)]
             
         return rand_response
-print(get_vector('hello'))
+a = get_vector('pigeon')
+b = get_vector('sparrow')
+print(compare_words(a,b))
 
-sent = "wow you're actually an idiot"
+sent = "i like pizza"
 C = Chatbot(sent,5)
 print(C.generate_response())
 

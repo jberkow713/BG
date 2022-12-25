3214b1bbb4a0271135d9b38e7ddc5894755b5157
jesse berkowitz <jberkow@brandeis.edu>
2022-12-24 23:38:52 -0800

    initial GPT2 functionality

---
+++ NLG.py
@@ -0,0 +1,45 @@
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import string
+import random
+import numpy as np
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+def remove_punc(sent):
+    # Removes punctuation given sentence
+    test_str = sent.translate(str.maketrans('', '', string.punctuation))
+    return test_str
+
+def get_vector(word):
+    # Gets specific word vector for GPT2 model
+    text_index = tokenizer.encode(word,add_prefix_space=True)
+    vector = model.transformer.wte.weight[text_index,:]
+    return vector[0].detach().numpy()
+
+class Chatbot:
+    # Initial version of chatbot
+    def __init__(self, input, min_reply):
+        self.input = input
+        self.min_reply = min_reply
+    def generate_response(self):
+        inputs = tokenizer.encode(self.input, return_tensors='pt')
+        outputs = model.generate(inputs, max_length=100, do_sample=True, temperature=.3,top_k=50)
+        text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        original = list(set(text.split('.')))        
+        final = [x for x in original if x!=remove_punc(self.input) and len(x)>0 and '\n' not in x and len(x.split())>self.min_reply]
+        if len(final)==0:
+            return "I don't know what to tell you..."
+
+        rand_response = final[random.randint(0,len(final)-1)]
+            
+        return rand_response
+print(get_vector('hello'))
+
+sent = "wow you're actually an idiot"
+C = Chatbot(sent,5)
+print(C.generate_response())
+
+
+

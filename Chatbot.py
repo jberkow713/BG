70cc7bd6f2faf162d4d6260ff1b965c8f171fa00
jesse berkowitz <jberkow@brandeis.edu>
2022-12-27 00:58:09 -0800

    spacy vocab added

--- Chatbot.py
+++ Chatbot.py
@@ -8,32 +8,35 @@ from transformers import BertTokenizer, BertModel
 import numpy as np
 from scipy import spatial
 import json
+import io
 
-nlp = spacy.load("en_core_web_sm")
-model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
-bert = BertTokenizer.from_pretrained('bert-base-uncased')
-model.eval()
-words = [x for x in bert.vocab.keys()]
+
+
+nlp = spacy.load("en_core_web_lg")
+# model = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+# bert = BertTokenizer.from_pretrained('bert-base-uncased')
+# model.eval()
+# words = [x for x in bert.vocab.keys()]
 
 def find_types(word_list, tenses):
     # Finds tense specified words from given word list and list of tenses    
     doc = nlp(' '.join(word_list))
-    return [x.text for x in doc if x.text.isalpha()==True and len(x.text)>1 and x.pos_ in tenses]    
+    return [x.text for x in doc if x.text.isalpha()==True and x.text in word_list and len(x.text)>1 and x.pos_ in tenses]
 
-def bert_vectors(sent):
+def find_words(tenses):
+    words = list(nlp.vocab.strings)[:50000]
+    doc = nlp(' '.join(words))
+    
+    return [x.text for x in doc if x.text.isalpha()==True and len(x.text)>1 and x.pos_ in tenses]
+
+def bert_vectors(word):
     # Creates contextualized vectors for given sentence
-    d = {}      
-    tokenized_text = bert.tokenize(sent)
+    tokenized_text = bert.tokenize(word)
     token_ids = bert.convert_tokens_to_ids(tokenized_text)
     tokens_tensor = torch.tensor([token_ids])    
     with torch.no_grad():
         outputs = model(tokens_tensor)    
-    w = [words[x] for x in token_ids]
-    count = 0
-    while count < len(w):
-        d[w[count]]=np.array(outputs[0][0][count])
-        count+=1    
-    return d
+    return np.array(outputs[0][0][0])    
 
 def compare_words(a,b):
     #compares word vectors using Bert model 
@@ -68,14 +71,56 @@ class Converter:
     def stem_sent(self,sent):
         return [self.stem(token) for token in self.tokenize(sent) if token not in self.stopwords]
 
-with open("Bert_Nouns.json") as file:
-    Nouns = json.load(file)
+def closest_vals(list, index,top_n):
 
-Compared = {}
-First = Nouns[0]
-for noun in Nouns[1:10]:
-    Compared[noun]=compare_words(bert_vectors(First)[First], bert_vectors(noun)[noun])
-print(Compared)    
+    Compared = {}
+    Word = list[index]
+    list.remove(Word)
+    count = 0
+    for word in list:
+        Compared[word]=compare_words(Word,word)
+        count+=1
+        if count %100 == 0:
+            print(count)
+    Top_Vals = sorted([x for x in Compared.values()],reverse=True)[:top_n]
+    Close = {}
+    Closest = []
+    for val in Top_Vals:
+        for k,v in Compared.items():
+            if v == val:
+                Closest.append(k)
+    Close[Word]=Closest
+    return Close
+
+
+# create_json('Spacy_Adjs.json', find_words(['ADJ']))
+# create_json('Spacy_Nouns.json', find_words(['NOUN']))
+# create_json('Spacy_Verbs.json', find_words(['VERB']))
+
+# with open("Spacy_Nouns.json") as file:
+#     Nouns = json.load(file)
+
+# print(Nouns[0:100])
+
+
+sent = 'airplane'
+sent_2 = 'helicopter'
+doc = nlp(sent)
+doc2 = nlp(sent_2)
+a = doc[0].vector
+b = doc2[0].vector
+print(compare_words(a,b))
+
+
+
+# with open("Bert_Nouns.json") as file:
+#     Nouns = json.load(file)
+
+# print(compare_words('time', 'years'))
+
+# print(closest_vals(Nouns,1,20))
+
+    
 
 
 

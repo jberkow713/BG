e18a0066c823bcb338894eccb2f9dbc5f5e4e7ae
jesse berkowitz <jberkow@brandeis.edu>
2022-12-28 13:36:15 -0800

    adding spacy vectors to analysis

--- NLG.py
+++ NLG.py
@@ -4,6 +4,9 @@ import string
 import random
 import numpy as np
 from scipy import spatial
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 tokenizer.add_special_tokens({
@@ -21,11 +24,17 @@ def remove_punc(sent):
     test_str = sent.translate(str.maketrans('', '', string.punctuation))
     return test_str
 
-def get_vector(word):
+def get_vector(word,model_type):
+    if model_type=='GPT':
+        text_index = tokenizer.encode(word,add_prefix_space=True)
+        vector = model.transformer.wte.weight[text_index,:]
+        return vector[0].detach().numpy()
+    elif model_type == 'SPACY':
+        vector = nlp(word)[0].vector
+        return vector
+        
     # Gets specific word vector for GPT2 model
-    text_index = tokenizer.encode(word,add_prefix_space=True)
-    vector = model.transformer.wte.weight[text_index,:]
-    return vector[0].detach().numpy()
+    
 
 def compare_words(a,b):
     #compares word vectors using Bert model 
@@ -37,6 +46,7 @@ def avg_vectors(vec_list):
 
 class Chatbot:
     # Initial version of chatbot
+    
     def __init__(self, input, min_reply,randomness):
         self.input = input
         self.min_reply = min_reply
@@ -53,14 +63,12 @@ class Chatbot:
         rand_response = final[random.randint(0,len(final)-1)]            
         return rand_response
 
-l = []
-a = get_vector('pigeon')
-b = get_vector('sparrow')
-l.append(a)
-l.append(b)
+# a = get_vector('pigeon','SPACY')
+# b = get_vector('sparrow', 'GPT')
 
-print(avg_vectors(l))
+sent = "I love watching television with my pants off in delaware"
+vectors = [get_vector(x, 'SPACY') for x in sent.split()]
+print(vectors)
 
-sent = "I love watching television with my pants off"
 C = Chatbot(sent,4,1.2)
 print(C.generate_response())

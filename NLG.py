ff468dacb1c3b19add03d54ce18665fa64b15f70
jesse berkowitz <jberkow@brandeis.edu>
2022-12-28 15:17:52 -0800

    cleaned up spacing

--- NLG.py
+++ NLG.py
@@ -6,8 +6,8 @@ import numpy as np
 from scipy import spatial
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
-
+Spacy= spacy.load("en_core_web_sm")
+Gpt= GPT2LMHeadModel.from_pretrained('gpt2')
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 tokenizer.add_special_tokens({
     "eos_token": "</s>",
@@ -17,8 +17,6 @@ tokenizer.add_special_tokens({
     "mask_token": "<mask>"
 })
 
-model = GPT2LMHeadModel.from_pretrained('gpt2')
-
 def remove_punc(sent):
     # Removes punctuation given sentence
     test_str = sent.translate(str.maketrans('', '', string.punctuation))
@@ -27,14 +25,11 @@ def remove_punc(sent):
 def get_vector(word,model_type):
     if model_type=='GPT':
         text_index = tokenizer.encode(word,add_prefix_space=True)
-        vector = model.transformer.wte.weight[text_index,:]
+        vector = Gpt.transformer.wte.weight[text_index,:]
         return vector[0].detach().numpy()
     elif model_type == 'SPACY':
-        vector = nlp(word)[0].vector
-        return vector
-        
-    # Gets specific word vector for GPT2 model
-    
+        vector = Spacy(word)[0].vector
+        return vector    
 
 def compare_words(a,b):
     #compares word vectors using Bert model 
@@ -45,30 +40,27 @@ def avg_vectors(vec_list):
     return Avg_Vector
 
 class Chatbot:
-    # Initial version of chatbot
-    
+    # Initial version of chatbot    
     def __init__(self, input, min_reply,randomness):
         self.input = input
         self.min_reply = min_reply
         self.randomness=randomness
+
     def generate_response(self):
         inputs = tokenizer.encode(self.input, return_tensors='pt')
-        outputs = model.generate(inputs, max_length=50, do_sample=True, temperature=self.randomness,top_k=50)
+        outputs = Gpt.generate(inputs, max_length=50, do_sample=True, temperature=self.randomness,top_k=50)
         text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        original = list(set(text.replace('\n','').split('.')))
-                   
+        original = list(set(text.replace('\n','').split('.')))                   
         final = [x for x in original if x!=remove_punc(self.input) and len(x)>0 and len(x.split())>self.min_reply]
         if len(final)==0:
             return "I don't know what to tell you..."
         rand_response = final[random.randint(0,len(final)-1)]            
         return rand_response
 
-# a = get_vector('pigeon','SPACY')
-# b = get_vector('sparrow', 'GPT')
-
 sent = "I love watching television with my pants off in delaware"
 vectors = [get_vector(x, 'SPACY') for x in sent.split()]
 print(vectors)
-
 C = Chatbot(sent,4,1.2)
 print(C.generate_response())
+# a = get_vector('pigeon','SPACY')
+# b = get_vector('sparrow', 'GPT')

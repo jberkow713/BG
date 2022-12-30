9560375fb1a4cbd57aafc8a44d96dcc10dc3639b
jesse berkowitz <jberkow@brandeis.edu>
2022-12-30 15:11:02 -0800

    grid

--- Word_Game.py
+++ Word_Game.py
@@ -24,41 +24,63 @@ p.init()
 clock = p.time.Clock()
 
 class Game:
-    def __init__(self,width,height,buffer):
+    def __init__(self,width,height,color):
         self.width = width
         self.height = height
         self.screen = p.display.set_mode((self.width, self.height))
+        self.color = color
         self.screen.fill(WHITE)
-        self.buffer = buffer
-
+        
     def draw_line(self, color, x1,y1,x2,y2):
         p.draw.line(self.screen, color, (x1, y1), (x2, y2))
     
     def draw_square(self, color,x1,y1,size):
         curr_x = x1
         curr_y = y1
-        for i in range(2):
+        for _ in range(2):
             final_x = curr_x+size
             final_y = curr_y   
             self.draw_line(color,curr_x,curr_y,final_x,final_y)
             curr_y+=size
         curr_x = x1
         curr_y = y1
-        for i in range(2):           
+        for _ in range(2):           
             final_y = curr_y+size
             final_x=curr_x   
             self.draw_line(color,curr_x,curr_y,final_x,final_y)
             curr_x+=size
-    def draw_grid(self, color, squares, x,y,size):
+
+    def draw_horizontal_squares(self, color, squares, x,y,size):
                 
         curr_x = x
-        for i in range(squares):
+        for _ in range(squares):
             self.draw_square(color,curr_x,y, size)
-            curr_x += size + self.buffer
+            curr_x += size 
+    
+    def draw_vertical_squares(self, color, squares, x,y,size):
+        curr_y = y
+        for _ in range(squares):
+            self.draw_square(color,x,curr_y, size)
+            curr_y += size 
+    
+    def draw_grid(self,squares,size):
+
+        start_x = (self.width - squares*size)/2
+        start_y = (self.height- squares*size)/2
+        for _ in range(2):
+            self.draw_horizontal_squares(self.color,squares,start_x,start_y,size)
+            start_y += (squares-1)*size
+        start_x = (self.width - squares*size)/2       
+        start_y = (self.height- squares*size)/2
+        for _ in range(2):
+            self.draw_vertical_squares(self.color,squares,start_x,start_y,size)
+            start_x += (squares-1)*size      
 
 
-G = Game(800,800,10)
-G.draw_grid(BLUE,5,100,100,100)
+
+
+G = Game(800,800,BLUE)
+G.draw_grid(8,80)
 
 while True:    
     
